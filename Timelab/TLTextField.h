5b33c56a12dee1c48297d7ea683245013e1a15fb
Paul Saumets <psaumets@gmail.com>
2014-09-26 10:13:14 -0400

    added TLTextField class which subclasses NSTextField.

---
+++ Timelab/TLTextField.h
@@ -0,0 +1,13 @@
+//
+//  TLTextField.h
+//  Timelab
+//
+//  Created by Paul Saumets on 9/25/14.
+//  Copyright (c) 2014 Design Lab Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TLTextField : NSTextField
+
+@end
---
+++ Timelab/TLTextField.m
@@ -0,0 +1,19 @@
+//
+//  TLTextField.m
+//  Timelab
+//
+//  Created by Paul Saumets on 9/25/14.
+//  Copyright (c) 2014 Design Lab Inc. All rights reserved.
+//
+
+#import "TLTextField.h"
+
+@implementation TLTextField
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
